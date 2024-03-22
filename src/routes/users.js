9726ca02fe81a0f6dcad939d24a7f549ab2e6adf
JuanRG-20221039 <20221039@uthh.edu.mx>
2024-03-21 23:17:36 -0600

    modified:   src/routes/users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { actualizarUsuario, cambiarContrasena, crearUsuario, eliminarUsuario, obtenerUsuarioPorId, obtenerUsuarios } from '../controllers/users.js';
+import { actualizarUsuario, crearUsuario, eliminarUsuario, obtenerUsuarioPorId, obtenerUsuarios } from '../controllers/users.js';
 
 export const usersRouter = Router();
 
@@ -9,4 +9,3 @@ usersRouter.post('', crearUsuario);
 usersRouter.get('/:id', obtenerUsuarioPorId);
 usersRouter.put('/:id', actualizarUsuario);
 usersRouter.delete('/:id', eliminarUsuario);
-usersRouter.put('/:id/cambiar-contrasena', cambiarContrasena);
